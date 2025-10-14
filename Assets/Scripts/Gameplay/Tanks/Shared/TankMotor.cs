ef18fc027cab098e86f7bafe7d06fe47dbf199f1
ItamarAlon <72469824+ItamarAlon@users.noreply.github.com>
2025-10-14 16:14:45 +0300

    Now Player Tank Hull rotates towards moving direction

--- Assets/Scripts/Gameplay/Tanks/Shared/TankMotor.cs
+++ Assets/Scripts/Gameplay/Tanks/Shared/TankMotor.cs
@@ -16,14 +16,36 @@ namespace Game.Gameplay.Tanks.Shared
 
         void Awake() 
         { 
-            _rb = GetComponent<Rigidbody2D>(); 
+            _rb = GetComponent<Rigidbody2D>();
+            //_rb.freezeRotation = true;
         }
         public void SetDesiredVelocity(Vector2 v) => _desiredVel = v * moveSpeed;
 
         void FixedUpdate()
         {
+            rotateHullTowardsMovingDirection();
             var vel = Vector2.MoveTowards(_rb.linearVelocity, _desiredVel, accel * Time.fixedDeltaTime);
             _rb.linearVelocity = vel;
+        }
+
+        private void rotateHullTowardsMovingDirection()
+        {
+            if (_desiredVel == Vector2.zero) return;
+
+            Quaternion desiredLookRotation;
+
+            //if (_desiredVel == Vector2.up || 
+            //    _desiredVel == Vector2.down ||
+            //    _desiredVel == Vector2.left ||
+            //    _desiredVel == Vector2.right)
+            //{
+            //    _rb.MoveRotation(Quaternion.identity);
+
+            //}
+            desiredLookRotation = Quaternion.LookRotation(_desiredVel);
+            Quaternion currentLookRotation = Quaternion.LookRotation(_rb.linearVelocity);
+            Debug.Log(Quaternion.Angle(desiredLookRotation, currentLookRotation));
+            _rb.MoveRotation(Quaternion.RotateTowards(currentLookRotation, desiredLookRotation, 3));
         }
     }
 }
