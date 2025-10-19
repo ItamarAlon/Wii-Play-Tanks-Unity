837d4955fc9351eeac975f847c303dd96eb4334c
ItamarAlon <72469824+ItamarAlon@users.noreply.github.com>
2025-10-20 00:12:42 +0300

    done

--- Assets/Scripts/UI/HUDController.cs
+++ Assets/Scripts/UI/HUDController.cs
@@ -1,6 +1,3 @@
-// Auto-generated stubs for Wii Play Tanks
-// Generated: 2025-10-03T10:21:31.691001
-// You can safely replace any class body with the real implementation from the HTML guide.
 
 using UnityEngine;
 using UnityEngine.UI;
